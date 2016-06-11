665f43b7ca117483b29e440c9ddb9514f8517f76
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2016-06-11 17:47:22 -0400

    initial app done

---
+++ app.js
@@ -0,0 +1,96 @@
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// create Oscillator and gain node
+var oscillator = audioCtx.createOscillator();
+var gainNode = audioCtx.createGain();
+
+// connect oscillator to gain node to speakers
+
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
+
+// create initial theremin frequency and volumn values
+
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+var maxFreq = 6000;
+var maxVol = 0.02;
+
+var initialFreq = 3000;
+var initialVol = 0.001;
+
+// set options for the oscillator
+
+oscillator.type = 'square';
+oscillator.frequency.value = initialFreq; // value in hertz
+oscillator.detune.value = 100; // value in cents
+oscillator.start(0);
+
+oscillator.onended = function() {
+  console.log('Your tone has now stopped playing!');
+}
+
+gainNode.gain.value = initialVol;
+
+// Mouse pointer coordinates
+
+var curX;
+var curY;
+var curZ;
+
+// Get new mouse pointer coordinates when mouse is moved
+// then set new gain and pitch values
+
+document.onmousemove = updatePage;
+
+function updatePage(event) {
+    curX = event.beta;
+    curY = event.gamma;
+    curZ = event.alpha;
+
+    console.log(event);
+    oscillator.frequency.value = (curX/WIDTH) * maxFreq;
+    gainNode.gain.value = (curY/HEIGHT) * maxVol;
+
+    //canvasDraw();
+}
+
+
+// canvas visualization
+/*
+function random(number1,number2) {
+  var randomNo = number1 + (Math.floor(Math.random() * (number2 - number1)) + 1);
+  return randomNo;
+}
+
+var canvas = document.querySelector('.canvas');
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+var canvasCtx = canvas.getContext('2d');
+
+function canvasDraw() {
+  if(KeyFlag == true) {
+    rX = KeyX;
+    rY = KeyY;
+  } else {
+    rX = CurX;
+    rY = CurY;
+  }
+  rC = Math.floor((gainNode.gain.value/maxVol)*30);
+
+  canvasCtx.globalAlpha = 0.2;
+
+  for(i=1;i<=15;i=i+2) {
+    canvasCtx.beginPath();
+    canvasCtx.fillStyle = 'rgb(' + 100+(i*10) + ',' + Math.floor((gainNode.gain.value/maxVol)*255) + ',' + Math.floor((oscillator.frequency.value/maxFreq)*255) + ')';
+    canvasCtx.arc(rX+random(0,50),rY+random(0,50),rC/2+i,(Math.PI/180)*0,(Math.PI/180)*360,false);
+    canvasCtx.fill();
+    canvasCtx.closePath();
+  }
+}
+*/
+
+window.addEventListener('deviceorientation', updatePage);
